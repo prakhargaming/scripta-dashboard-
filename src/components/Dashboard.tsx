9ad86430c5cdd32676559ce2af3fda17587ab776
prakhargaming <psinha@ucdavis.edu>
2024-07-22 08:58:21 -0700

    updated modal css

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,6 +4,7 @@
 
 import React, { useState } from 'react';
 import { FaFileAlt, FaCheck, FaExclamation, FaTimes } from 'react-icons/fa';
+import { CiSettings } from "react-icons/ci";
 import ItemsCol from '../components/Itemscol';
 import Sidebar from './Sidebar';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../components/Table";
@@ -69,15 +70,27 @@ export default function Dashboard() {
         <div className="h-6"></div>
         <div className="grid grid-cols-1 md:grid-cols- lg:grid-cols-2 gap-6">
           <ItemsCol resumes={resumes} title="Unfiltered Resumes (In Progress)" numero={3} icon={FaExclamation} setModalInfo={setModalInfo} onTitleClick={handleTitleClick} />
-          <div className='glassCard2' />
+          <div className="glassCard2 shadow-2xl">
+            <div className="p-8">
+              <div className="flex items-center justify-between px-4 py-3">
+                <div className="flex items-center">
+                  <CiSettings className="text-white mr-5 w-9 h-9" />
+                  <span 
+                    className="text-3xl font-light text-white cursor-pointer hover:underline" >
+                    Settings
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
       {/* Existing modal for resume details */}
       {modalInfo && (
-        <div id="default-modal" className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative p-4 w-full max-w-7xl h-5/6">
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 h-full flex flex-col">
+        <div id="default-modal" className="fixed inset-0 z-50 flex backdrop-blur items-center justify-center bg-black bg-opacity-50">
+          <div className="relative w-full max-w-7xl h-5/6 glassCard3 p-7"> 
+            <div className="relative h-full flex flex-col">
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">{modalInfo.name}</h3>
                 <button
@@ -122,57 +135,62 @@ export default function Dashboard() {
         </div>
       )}
 
-     {/* New modal for all resumes */}
-     {showAllResumes && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative p-4 w-full max-w-4xl max-h-[80vh] overflow-y-auto">
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                  All Resumes
-                </h3>
-                <button
-                  type="button"
-                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                  onClick={() => setShowAllResumes(false)}
-                >
-                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                  </svg>
-                  <span className="sr-only">Close modal</span>
-                </button>
-              </div>
-              <div className="p-4 md:p-5 space-y-4">
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead className="w-[200px]">Name</TableHead>
-                      <TableHead>Status</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {resumes.map((resume, index) => (
-                      <TableRow key={index}>
-                        <TableCell className="font-big text-lg text-white">
-                          <button
-                            className="text-blue-500 hover:underline"
-                            onClick={() => {
-                              setModalInfo(resume);
-                              setShowAllResumes(false);
-                            }}
-                          >
-                            {resume.name}
-                          </button>
-                        </TableCell>
-                        <TableCell>
-                          <div className={`w-3 h-3 rounded-full ${getStatusColor(resume.status)}`}></div>
-                        </TableCell>
+
+      {/* New modal for all resumes */}
+      {showAllResumes && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black backdrop-blur bg-opacity-50">
+          <div className="relative w-full max-w-4xl h-[80vh] glassCard3 p-7 flex flex-col">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-3xl text-gray-900 dark:text-white">
+                All Resumes
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                onClick={() => setShowAllResumes(false)}
+              >
+                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+            <div className="flex-grow overflow-y-auto p-4 md:p-5 no-scrollbar">
+              <div className="flex justify-center">
+                <div className="w-full max-w-2xl">
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[200px]">Name</TableHead>
+                        <TableHead>Status</TableHead>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
+                    </TableHeader>
+                    <TableBody>
+                      {resumes.map((resume, index) => (
+                        <TableRow key={index}>
+                          <TableCell className="font-big text-lg text-white">
+                            <button
+                              className="text-blue-500 hover:underline"
+                              onClick={() => {
+                                setModalInfo(resume);
+                                setShowAllResumes(false);
+                              }}
+                            >
+                              {resume.name}
+                            </button>
+                          </TableCell>
+                          <TableCell>
+                            <div className={`w-3 h-3 rounded-full ${getStatusColor(resume.status)}`}></div>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
               </div>
-              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+            </div>
+            <div className="p-4 md:p-5 border-t border-gray-200 dark:border-gray-600">
+              <div className="flex justify-center">
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
